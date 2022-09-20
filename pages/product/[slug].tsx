986c23c5e2f1fb53fe55574c0aa0012707606ef5
Albert Chang <a89529294@hotmail.com>
2022-09-20 14:30:23 +0800

    feat: finish section 4 vid 5

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -2,7 +2,15 @@ import Image from "next/future/image";
 import { useRouter } from "next/router";
 import React from "react";
 import { useQuery } from "urql";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
 import { GET_PRODUCT_QUERY } from "../../lib/query";
+import {
+  Buy,
+  DetailsStyle,
+  ProductInfo,
+  Quantity,
+} from "../../styles/ProductDetails";
 
 function ProductDetails() {
   const {
@@ -14,24 +22,47 @@ function ProductDetails() {
   });
   if (fetching) return <h1>loading...</h1>;
   if (error) return <h1>Error: {error.message}</h1>;
-
-  console.log(data.products.data[0].attributes);
+  if (!data.products.data.length) return <h1>Product does not exist!</h1>;
+  const {
+    title,
+    description,
+    image: {
+      data: {
+        attributes: {
+          formats: {
+            medium: { url },
+          },
+        },
+      },
+    },
+  } = data.products.data[0].attributes;
 
   return (
-    <div>
-      {/* <Image /> */}
-      <div>
-        <h3>Title</h3>
-        <p>descrip</p>
-      </div>
-      <div>
-        <span>Quant</span>
-        <button>plus</button>
-        <p>0</p>
-        <button>minus</button>
-      </div>
-      <button>Add to cart</button>
-    </div>
+    <DetailsStyle>
+      <Image
+        src={url}
+        alt={title}
+        width={500}
+        height={1}
+        style={{ width: "40%", height: "auto" }}
+      />
+      <ProductInfo>
+        <h3>{title}</h3>
+        <p>{description}</p>
+
+        <Quantity>
+          <span>Quant</span>
+          <button>
+            <AiFillMinusCircle />
+          </button>
+          <p>0</p>
+          <button>
+            <AiFillPlusCircle />
+          </button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </ProductInfo>
+    </DetailsStyle>
   );
 }
 
